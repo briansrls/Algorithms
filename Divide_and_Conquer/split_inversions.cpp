b84175444823c967fd3308251f797178f093648a
Brian Searls <briansrls@gmail.com>
2016-12-07 20:21:51 -0500

    Finished Split Inversions

--- Divide_and_Conquer/split_inversions.cpp
+++ Divide_and_Conquer/split_inversions.cpp
@@ -12,12 +12,20 @@ void printVector(vector<int> a){
 }
 
 template <typename T>
-int countSplitInversions(vector<T>& array1, vector<T>& array2){
+int merge(vector<T>& array, vector<T>& array1, vector<T>& array2){
 	//need to copy contents of array1 and 2 into array
 	int i, j;
+	int inversions = 0;
 	for(i = 0, j = 0; i < array1.size(), j < array2.size(); ){
-		i++;
-		j++;
+		if(array1[i] < array2[j]){
+			array[i+j] = array1[i];
+			i++;
+		}
+		else{
+			array[i+j] = array2[j];
+			j++;
+			inversions+=array1.size()-i;
+		}
 	}
 	
 	while(i < array1.size()){
@@ -27,28 +35,31 @@ int countSplitInversions(vector<T>& array1, vector<T>& array2){
 	while(j < array2.size()){
 		array[i+j] = array1[j];
 		j++;
+		inversions++;
 	}
-	
+	return inversions;	
 	
 }
 
 template <typename T>
-int split(vector<T>& array) {
+int merge_sort(vector<T>& array) {
 	if (1 < array.size()) {
+		int inversions = 0;
 		vector<T> array1(array.begin(), array.begin() + array.size() / 2);
-		split(array1);
+		inversions+=merge_sort(array1);
 		vector<T> array2(array.begin() + array.size() / 2, array.end());
-		split(array2);
-		return countSplitInversions(array1, array2);
+		inversions+=merge_sort(array2);
+		return inversions+merge(array, array1, array2);
 	}
 }
 
 int main(){
-	vector<int> a(25, 0);
+	vector<int> a(26, 0);
 	for(int i = 0; i < a.size(); i++){
 		a[i] = a.size() -1 -i;
 	}
-	merge_sort(a);
+	printVector(a);
+	cout << "Inversions = " << merge_sort(a) << std::endl;
 	printVector(a);
 
 }
